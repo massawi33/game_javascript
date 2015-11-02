6e069ef9bf3dd914870540c181a5793ee3babef0
Zakaria HZA <zakaria.hza@gmail.com>
2015-11-02 15:52:17 +0100

    10th question

--- src/engin.js
+++ src/engin.js
@@ -1,67 +1,121 @@
 /**
  * Created by User on 01/10/2015.
  */
-var engin ;
+var engin;
 engin = function () {
     //function ma_function(p1, p2) {
-        //return 0;
+    //return 0;
     //}
 
-     var carre = new Array(3);
-     var joueur ;
-   //carre = new Array(["b","b","b"] , ["b","b","b"] );
+    'use strict';
+    var carre = new Array(3), joueur, carre2 = new Array(3), carre3 = new Array(3), carre4 = new Array(3);
+    //carre = new Array(["b","b","b"] , ["b","b","b"] );
 
-    this.init = function() {
+    this.init = function () {
 
-        var i;
-        var j;
-        var hehe = 1;
-        for (i = 0; i < 3; i++) {
+        var i, j;
+        //var hehe = 1;
+        for (i = 0; i < 3; i += 1) {
             carre[i] = new Array(3);
-            for (j = 0; j < 3; j++) {
+            carre2[i] = new Array(3);
+            carre3[i] = new Array(3);
+            carre4[i] = new Array(3);
+            for (j = 0; j < 3; j += 1) {
                 //carre[i][j] = false;
-                carre[i][j] = "vide" ;
+                carre[i][j] = "vide";
+                carre2[i][j] = "vide";
+                carre3[i][j] = "vide";
+                carre4[i][j] = "vide";
                 //hehe++;
                 // return carre[i][j];
             }
         }
+
 
         //-----------------------------------------------
 
         joueur = "Blanc";
 
     };
-    this.check_joueur = function() {
-            return joueur;
+    this.check_joueur = function () {
+        return joueur;
     };
-    this.start_blanc = function() {
-        carre [0][0] = "b";
+    this.start_blanc = function () {
+        carre[0][0] = "b";
 
     };
-    this.check_start_blanc = function(){
-        return carre[0][0] ;
+    this.check_start_blanc = function () {
+        return carre[0][0];
     };
-    this.change_player = function(){
-        if(joueur === "Blanc"){
+    this.change_player = function () {
+        if (joueur === "Blanc") {
 
             joueur = "Noir";
-        }
-        else if (joueur === "Noir"){
+        } else if (joueur === "Noir") {
 
             joueur = "Blanc";
         }
 
     };
-    this.display_carre = function(){
-        var i ;
-        var j ;
+    this.display_carre = function () {
+        var i, j;
 
-        for(i = 0 ; i < 3 ; i++){
+        for (i = 0; i < 3; i += 1) {
 
-            for(j = 0 ; j < 3 ; j++){
+            for (j = 0; j < 3; j += 1) {
 
-                console.log(carre [i][j] + " " + i + " " + j);
+                console.log(carre[i][j] + " " + i + " " + j);
 
+
+            }
+
+
+        }
+
+
+    };
+    this.display_carre2 = function () {
+        var i, j;
+
+        for (i = 0; i < 3; i += 1) {
+
+            for (j = 0; j < 3; j += 1) {
+
+                console.log(carre2[i][j] + " " + i + " " + j);
+
+
+            }
+
+
+        }
+
+
+    };
+    this.display_carre3 = function () {
+        var i, j;
+
+        for (i = 0; i < 3; i += 1) {
+
+            for (j = 0; j < 3; j += 1) {
+
+                console.log(carre3[i][j] + " " + i + " " + j);
+
+
+            }
+
+
+        }
+
+
+    };
+    this.display_carre4 = function () {
+        var i, j;
+
+        for (i = 0; i < 3; i += 1) {
+
+            for (j = 0; j < 3; j += 1) {
+
+                console.log(carre4[i][j] + " " + i + " " + j);
 
 
             }
@@ -72,109 +126,240 @@ engin = function () {
 
     };
 
-
-    this.rotation_90_clockwise = function(){
-        var pr = new Array(3);
-        var i;
-        var pivot;
-        var j;
-        for (i = 0; i < 3; i++) {
+    this.rotation_90_clockwise = function (cr) {
+        var pr = new Array(3), i, pivot, j;
+        for (i = 0; i < 3; i += 1) {
             pr[i] = new Array(3);
-            for (j = 0; j < 3; j++) {
+            for (j = 0; j < 3; j += 1) {
                 pr[i][j] = "vide";
 
             }
         }
+        if (cr === 1) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[pivot][i] = carre[i][j];
+                    pivot += 1;
 
-        for(i = 0 ; i < 3 ; i++){
-            pivot = 0;
-            for(j = 2 ; j > -1 ; j--){
-                pr[pivot][i] = carre [i][j];
-                pivot ++;
+
+                }
 
 
             }
 
 
-        }
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre[i][j] = pr[i][j];
+                }
 
 
-        for(i = 0 ; i < 3 ; i++){
+            }
+        } else if (cr === 2) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[pivot][i] = carre2[i][j];
+                    pivot += 1;
 
-            for(j = 0 ; j < 3 ; j++){
 
-                carre [i][j] = pr[i][j];
+                }
+
+
             }
 
 
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre2[i][j] = pr[i][j];
+                }
+
+
+            }
+        } else if (cr === 3) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[pivot][i] = carre3[i][j];
+                    pivot += 1;
+
+
+                }
+
+
+            }
+
+
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre3[i][j] = pr[i][j];
+                }
+
+
+            }
+        } else if (cr === 4) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[pivot][i] = carre4[i][j];
+                    pivot += 1;
+
+
+                }
+
+
+            }
+
+
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre4[i][j] = pr[i][j];
+                }
+
+
+            }
         }
 
         //this.display_carre();
 
 
     };
-    this.rotation_90_counterclockwise = function(){
+    this.rotation_90_counterclockwise = function (cr) {
 
-        var pr = new Array(3);
-        var i;
-        var pivot;
-        var j;
-        for (i = 0; i < 3; i++) {
+        var pr = new Array(3), i, pivot, j;
+        for (i = 0; i < 3; i += 1) {
             pr[i] = new Array(3);
-            for (j = 0; j < 3; j++) {
+            for (j = 0; j < 3; j += 1) {
                 pr[i][j] = "vide";
 
             }
         }
+        if (cr === 1) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[i][pivot] = carre[j][i];
+                    pivot += 1;
 
-        for(i = 0 ; i < 3 ; i++){
-            pivot = 0;
-            for(j = 2 ; j > -1 ; j--){
-                pr[i][pivot] = carre [j][i];
-                pivot ++;
+
+                }
 
 
             }
 
 
-        }
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre[i][j] = pr[i][j];
+                }
 
 
-        for(i = 0 ; i < 3 ; i++){
+            }
+        } else if (cr === 2) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[i][pivot] = carre2[j][i];
+                    pivot += 1;
 
-            for(j = 0 ; j < 3 ; j++){
 
-                carre [i][j] = pr[i][j];
+                }
+
+
             }
 
 
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre2[i][j] = pr[i][j];
+                }
+
+
+            }
+        } else if (cr === 3) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[i][pivot] = carre3[j][i];
+                    pivot += 1;
+
+
+                }
+
+
+            }
+
+
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre3[i][j] = pr[i][j];
+                }
+
+
+            }
+        } else if (cr === 4) {
+            for (i = 0; i < 3; i += 1) {
+                pivot = 0;
+                for (j = 2; j > -1; j -= 1) {
+                    pr[i][pivot] = carre4[j][i];
+                    pivot += 1;
+
+
+                }
+
+
+            }
+
+
+            for (i = 0; i < 3; i += 1) {
+
+                for (j = 0; j < 3; j += 1) {
+
+                    carre4[i][j] = pr[i][j];
+                }
+
+
+            }
         }
         //this.display_carre();
 
 
     };
-    this.check_rotation_1 = function() {
-        return carre [2][0];
+    this.check_rotation_1 = function () {
+        return carre[2][0];
     };
-    this.check_rotation_black_2 = function() {
-        return carre [0][2];
+    this.check_rotation_black_2 = function () {
+        return carre[0][2];
     };
-    this.black_first_move = function(){
-        carre [0][0] = "n";
+    this.black_first_move = function () {
+        carre[0][0] = "n";
     };
-    this.check_black_first_move = function(){
+    this.check_black_first_move = function () {
 
         return carre[0][0];
 
     };
-    this.check = function() {
-        var i;
-        var j;
-        var num = 0;
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (carre[i][j] != "vide") {
-                    num++;
+    this.check = function () {
+        var i, j, num = 0;
+        for (i = 0; i < 3; i += 1) {
+            for (j = 0; j < 3; j += 1) {
+                if (carre[i][j] !== "vide") {
+                    num += 1;
                 }
 
                 // return carre[i][j];
@@ -183,43 +368,89 @@ engin = function () {
         return num;
 
     };
-    this.not_empty_exception = function(){
+    this.not_empty_exception = function () {
 
         this.name = "not_empty";
 
     };
 
-    this.blanc_play = function(a , b){
+    this.blanc_play = function (a, b, cr) {
+        if (cr === 1) {
+            if (carre[a][b] !== "vide") {
 
-        if(carre [a][b] != "vide"){
-
-            throw new this.not_empty_exception();
+                throw new this.not_empty_exception();
 
 
-        }
-        else
-        {
+            }
             carre[a][b] = "b";
+        } else if (cr === 2) {
+            if (carre2[a][b] !== "vide") {
 
+                throw new this.not_empty_exception();
+
+
+            }
+            carre2[a][b] = "b";
+        } else if (cr === 3) {
+            if (carre3[a][b] !== "vide") {
+
+                throw new this.not_empty_exception();
+
+
+            }
+            carre3[a][b] = "b";
+        } else if (cr === 4) {
+            if (carre4[a][b] !== "vide") {
+
+                throw new this.not_empty_exception();
+
+
+            }
+            carre4[a][b] = "b";
         }
+
+
 
 
     };
-    this.black_play = function(a , b){
+    this.black_play = function (a, b, cr) {
 
-        if(carre [a][b] != "vide"){
+        if (cr === 1) {
+            if (carre[a][b] !== "vide") {
 
-            throw new this.not_empty_exception();
+                throw new this.not_empty_exception();
 
 
-        }
-        else
-        {
+            }
             carre[a][b] = "n";
+        } else if (cr === 2) {
+            if (carre2[a][b] !== "vide") {
 
+                throw new this.not_empty_exception();
+
+
+            }
+            carre2[a][b] = "n";
+        } else if (cr === 3) {
+            if (carre3[a][b] !== "vide") {
+
+                throw new this.not_empty_exception();
+
+
+            }
+            carre3[a][b] = "n";
+        } else if (cr === 4) {
+            if (carre4[a][b] !== "vide") {
+
+                throw new this.not_empty_exception();
+
+
+            }
+            carre4[a][b] = "n";
         }
+
 
 
     };
 
-}
+};
--- test/test.js
+++ test/test.js
@@ -3,65 +3,87 @@
  */
 MonTestCase = TestCase("MonTestCase");
 
-a = new engin();
+var a = new engin();
 a.init();
 
 
-MonTestCase.prototype.testA = function() {
+MonTestCase.prototype.testA = function () {
 
-    assertTrue(a.check() === 0 );
+    assertTrue(a.check() === 0);
 };
-MonTestCase.prototype.testB = function() {
+MonTestCase.prototype.testB = function () {
 
-    assertTrue(a.check_joueur() === "Blanc" );
+    assertTrue(a.check_joueur() === "Blanc");
 
 };
-MonTestCase.prototype.testC = function() {
+MonTestCase.prototype.testC = function () {
     a.start_blanc();
-    assertTrue(a.check_start_blanc() === "b" );
+    assertTrue(a.check_start_blanc() === "b");
 
 };
 
-MonTestCase.prototype.testD = function() {
+MonTestCase.prototype.testD = function () {
     //a.start_blanc();
-    assertTrue(a.check() === 1 );
+    assertTrue(a.check() === 1);
 };
 
-MonTestCase.prototype.testE = function() {
+MonTestCase.prototype.testE = function () {
     //a.start_blanc();
     a.change_player();
-    assertTrue(a.check_joueur() === "Noir" );
+    assertTrue(a.check_joueur() === "Noir");
 };
 
-MonTestCase.prototype.testF = function() {
+MonTestCase.prototype.testF = function () {
     //a.start_blanc();
-    a.rotation_90_clockwise();
+    a.rotation_90_clockwise(1);
     //a.rotation_90_counterclockwise()
 
-    assertTrue(a.check_rotation_1() === "b" );
+    assertTrue(a.check_rotation_1() === "b");
 };
-MonTestCase.prototype.testG = function(){
+MonTestCase.prototype.testG = function () {
 
     a.black_first_move();
-    assertTrue(a.check() === 2 );
+    assertTrue(a.check() === 2);
     assertTrue(a.check_black_first_move() === "n");
 
 
 };
-MonTestCase.prototype.testH = function(){
+MonTestCase.prototype.testH = function () {
 
-    a.rotation_90_counterclockwise();
-    assertTrue(a.check_start_blanc() === "b" );
-    assertTrue(a.check_rotation_black_2() === "n" );
-
-
+    a.rotation_90_counterclockwise(1);
+    assertTrue(a.check_start_blanc() === "b");
+    assertTrue(a.check_rotation_black_2() === "n");
 
 
 };
-MonTestCase.prototype.testI = function(){
+/*MonTestCase.prototype.testI = function () {
 
     //assertTrue(a.blanc_play(0,0) === "not_empty" );
-    assertException( function(){a.blanc_play(0,0)},"not_empty");
+    assertException(function () {
+        a.blanc_play(0, 0);
+    }, "not_empty");
+
+
+};*/
+MonTestCase.prototype.test10 = function () {
+
+    //assertTrue(a.blanc_play(0,0) === "not_empty" );
+    //a.blanc_play(0,0,1);
+    //a.black_play(0,2,1);
+    a.blanc_play(1, 0, 1);
+    a.rotation_90_clockwise(1);
+    a.black_play(0, 1, 1);
+    a.rotation_90_counterclockwise(1);
+    a.blanc_play(2, 0, 1);
+    a.rotation_90_clockwise(1);
+    a.black_play(0, 2, 1);
+    a.rotation_90_counterclockwise(1);
+    a.blanc_play(0, 0, 2);
+    a.rotation_90_counterclockwise(2);
+    a.black_play(2, 2, 2);
+    a.rotation_90_clockwise(2);
+    a.display_carre();
+    a.display_carre2();
 
 
 
